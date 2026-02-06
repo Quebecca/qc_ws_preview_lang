3d71dc42f326f49910a6db0074c9260b39cad831
taieb <taieb@mokom01.com>
2026-02-06 18:31:29 +0100

    Remove unused code and update Readme

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,11 +2,6 @@
 
 defined('TYPO3') || die();
 
-/**
- * Override the original middleware just to add the new method of generate language service
- */
-$typoVersion = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Information\Typo3Version::class)->getMajorVersion();
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Workspaces\Middleware\WorkspacePreview::class] = [
     'className' => \Qc\QcWsPreviewLang\Middleware\ExtendedWorkspacePreview::class
 ];
@@ -14,7 +9,3 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Workspaces\Middleware\W
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Workspaces\Preview\PreviewUriBuilder::class] = [
     'className' => \Qc\QcWsPreviewLang\Middleware\ExtendedPreviewUriBuilder::class
 ];
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    "@import 'EXT:qc_ws_preview_lang/Configuration/TSconfig/pageconfig.tsconfig'"
-);
